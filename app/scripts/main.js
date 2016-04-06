af607a2be3dffb582e6ff7eca625ae1228a804f7
Jason L Perry <jasper@ambethia.com>
2016-04-06 03:19:58 -0400

    Invites, maybe

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@ const inputs = document.querySelectorAll('input');
 const closeButtons = document.querySelectorAll('.nevermind, header .thanks button');
 const header = document.querySelector('header');
 const form = document.querySelector('header form');
+const submitButton = document.querySelector('.invite');
 
 const positionModal = () => {
   if (document.body.classList.contains('invite-modal')) {
@@ -38,7 +39,8 @@ const closeModal = () => {
     form.elements[i].classList.remove('changed');
     form.elements[i].disabled = false;
   }
-  document.body.classList.remove('invite-modal', 'thanks');
+  submitButton.innerHTML = '<i class="fa fa-check"></i> Okay';
+  document.body.classList.remove('invite-modal', 'thanks', 'error');
 };
 
 const handleCloseButton = (event) => {
@@ -46,9 +48,26 @@ const handleCloseButton = (event) => {
   closeModal();
 };
 
+const handleInviteSuccess = () => {
+  document.body.classList.add('thanks');
+};
+
+const handleInviteError = (message) => {
+  submitButton.innerHTML = '<i class="fa fa-check"></i> Okay';
+  for (let i = 0; i < form.elements.length; i++) {
+    form.elements[i].disabled = false;
+  }
+  document.querySelector('.error p.message').innerHTML = message;
+  document.body.classList.add('error');
+};
+
+document.querySelector('header .error button[type=submit]').addEventListener('click', (event) => {
+  event.preventDefault();
+  document.body.classList.remove('error');
+});
+
 const handleInviteSubmit = (event) => {
   event.preventDefault();
-  const submitButton = document.querySelector('.invite');
 
   for (let i = 0; i < form.elements.length; i++) {
     form.elements[i].disabled = true;
@@ -58,7 +77,58 @@ const handleInviteSubmit = (event) => {
   submitButton.innerHTML = '<i class="fa fa-cog loading"></i>';
   submitButton.style.width = preservedWidth;
 
-  document.body.classList.add('thanks');
+  // DO INVITE
+  handleInviteError();
+
+  // $.ajax({
+  //     type: "POST",
+  //     url: 'https://' + settings.execute_api_domain + '/production/invite',
+  //     contentType: "application/json",
+  //     dataType: 'json',
+  //     data: JSON.stringify( { email : email_value } ),
+  //     success: function( data ) {
+  //         try {
+  //             data = JSON.parse( data );
+  //             if ( data.ok ) {
+  //                 handle_success( 'Great success' );
+  //             }
+  //             else {
+  //                 handle_error( 'Error while calling API: ' + data.error );
+  //             }
+  //         }
+  //         catch ( error ) {
+  //             handle_error( 'Unknown when parsing API return: ' + error );
+  //         }
+  //     },
+  //     error: function( xhr, status, error ) {
+  //         handle_error( 'Unknown transport error: ' + status + ( error ? ' ' + error : '' ) );
+  //     }
+  // });
+
+  try {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://oouple62r7.execute-api.us-east-1.amazonaws.com/production/invite');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+      email: form.emailAddress,
+      first_name: form.familyName,
+      last_name: form.givenName,
+    }));
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          handleInviteSuccess();
+        } else {
+          const data = JSON.parse(xhr.responseText);
+          handleInviteError(`Error: ${data.error}`);
+        }
+      } else {
+        handleInviteError(`XHR Error: ${xhr.status}`);
+      }
+    };
+  } catch (e) {
+    handleInviteError(`Unknown Error: ${e}`);
+  }
 };
 form.addEventListener('submit', handleInviteSubmit, false);
 
