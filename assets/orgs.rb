6b8c2be8eabe8b25d45a676a9d4d17ded1ef3661
Jason L Perry <jasper@ambethia.com>
2016-04-07 10:21:17 -0400

    New groups

--- assets/orgs.rb
+++ assets/orgs.rb
@@ -19,17 +19,21 @@ estimates = [
   Girl-Develop-It-Tampa-Bay
   Tampa-iOS-Meetup
   St-Pete-NET-Meetup
+  tampa-nodejs
+  TampaUnity3D
   suncoast-devs
   CoderNight
   GeekLunch
+  Clearwater-Dunedin-Geek-Breakfast
   GameCreators
 ].each do |urlname|
   open("https://api.meetup.com/#{urlname}?key=#{ENV['MEETUP_API_KEY']}") do |io|
     group = JSON.parse(io.read)
     members = ((group['members'].to_f / UPTO).ceil) * UPTO
+    thumb_url = group['group_photo'] ? group['group_photo']['thumb_link'] : group['photos'][0]['thumb_link']
     puts <<-EOF
           <li class="org">
-            <img src="#{group['group_photo']['thumb_link']}" alt="#{group['name']} Image"/>
+            <img src="#{thumb_url}" alt="#{group['name']} Image"/>
             <div class="info">
               <h3><a href="#{group['link']}">#{group['name']}</a></h3>
               <p>#{estimates.pop} #{members} #{group['who']}</p>
